8a2d58f292f95e9810e7df9ab3bd4e968195604c
Lenin Guananga <lenindgc@hotmail.com>
2022-09-05 20:49:01 -0500

    Taller 38 Programación Web - GET, POST, PUT y DELETE

--- src/main/java/com/uce/edu/demo/controller/PersonaController.java
+++ src/main/java/com/uce/edu/demo/controller/PersonaController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.uce.edu.demo.repository.modelo.Persona;
@@ -24,6 +28,40 @@ public class PersonaController {
 		
 		List<Persona> lista = this.personaService.buscarTodos();
 		modelo.addAttribute("personas", lista);
-		return "vistaListaEstudiantes";
+		return "vistaListaPersonas";
 	}
+	
+	@GetMapping("/buscarUno/{idPersona}") //{} = sera una variable con un nombre
+	public String buscarPersona(@PathVariable("idPersona") Integer id, Model modelo) {
+		System.out.println("El ID: " + id);
+		Persona per = this.personaService.buscarPorId(id);
+		modelo.addAttribute("persona", per);
+		return "vistaPersona";
+	}
+	
+	@PutMapping("/actualizar/{idPersona}")
+	public String actualizarPersona(@PathVariable("idPersona") Integer id, Persona persona) { //el nombre persona debe ser el mismo del th:object en el form
+		persona.setId(id);
+		this.personaService.actualizar(persona);
+		return "redirect:/personas/buscar";
+	}
+	
+	@DeleteMapping("/borrar/{idPersona}")
+	public String borrarPersona(@PathVariable("idPersona") Integer id) {
+		this.personaService.eliminar(id);
+		return "redirect:/personas/buscar";	
+	}
+	
+	@PostMapping("/insertar")
+	public String insertarPersona(Persona persona) {
+		this.personaService.guardar(persona);
+		return "redirect:/personas/buscar";
+	}
+	
+	//Metodo de redireccionamientos a paginas
+	@GetMapping("/nuevaPersona")
+	public String paginaNuevaPersona(Persona persona) { //siempre se debe mandar como alrgumento el objeto con el que se va a trabajar, aunque no se lo use
+		return "vistaNuevaPersona";
+	}
+	
 }
